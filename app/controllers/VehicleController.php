beb4ccf93193f08e47f282aac1f1adc22cfe7650
bvdon <bvdon@yahoo.com>
2020-02-03 05:55:16 -0500

    Update VehicleController.php

--- app/controllers/VehicleController.php
+++ app/controllers/VehicleController.php
@@ -9,7 +9,7 @@ class VehicleController extends Controller
         $this->VehicleModel = $this->model('VehicleModel');
     }
 
-    public function display($data)
+    public function display($data=1)
     {
         // get vehicle data[] from model, then render the view.
         $this->view('vehicle/index', $this->VehicleModel->getVehicleData($data));
